5c09c296237688e1e7a099871cfd2670582b6370
Ifte <156860282+Ifte-Ace@users.noreply.github.com>
2024-10-15 15:16:45 -0400

    Create Poe.java

---
+++ Poe.java
@@ -0,0 +1,13 @@
+
+public class Poe extends Kylo {
+ 
+public void method2() {
+ 
+super.method2();
+ 
+System.out.print("Poe 2 ");
+ 
+}
+public String toString() {
+ 
+return "Poe " + super.toString();
